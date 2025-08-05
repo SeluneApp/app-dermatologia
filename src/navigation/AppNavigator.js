5f9c7835c55fb581cc19906708f95db97a780be3
Selune <seluneApp@gmail.com>
2025-08-05 15:49:30 -0300

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import LoginScreen from '../screens/LoginScreen/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen/RegisterScreen';
-import ProfileScreen from '../screens/ProfileScreen/ProfileScree';
+import ProfileScreen from '../screens/ProfileScreen/ProfileScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomePageNoite from '../screens/HomePageNoite/HomePageNoite';
@@ -11,12 +11,11 @@ const Stack = createStackNavigator();
 export default function AppNavigator() {
   return (
     <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="HomePageNoite" component={HomePageNoite} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
-      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomePageNoite} />
       </Stack.Navigator>
     </NavigationContainer>
   );
